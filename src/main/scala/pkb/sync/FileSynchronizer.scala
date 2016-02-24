b1acd8922251d6a401afa758edc41d74d8473fb3
Tpt <thomaspt@hotmail.fr>
2016-02-24 14:07:10 +0100

    Allows FileSynchronizer to import a complete directory
    Adds support of .eml files

--- src/main/scala/pkb/sync/FileSynchronizer.scala
+++ src/main/scala/pkb/sync/FileSynchronizer.scala
@@ -1,17 +1,20 @@
 package pkb.sync
 
-import java.io.{File, IOException}
+import java.io.File
 
 import org.apache.commons.io.FilenameUtils
 import org.openrdf.model.impl.LinkedHashModel
 import org.openrdf.model.{Model, ValueFactory}
-import pkb.sync.converter.{ICalConverter, VCardConverter}
+import org.slf4j.LoggerFactory
+import pkb.sync.converter.{EmailMessageConverter, ICalConverter, VCardConverter}
 
 /**
   * @author Thomas Pellissier Tanon
   */
 class FileSynchronizer(valueFactory: ValueFactory) {
 
+  private val logger = LoggerFactory.getLogger(classOf[FileSynchronizer])
+  private val emailMessageConverter = new EmailMessageConverter(valueFactory)
   private val iCalConverter = new ICalConverter(valueFactory)
   private val vCardConverter = new VCardConverter(valueFactory)
 
@@ -21,13 +24,26 @@ class FileSynchronizer(valueFactory: ValueFactory) {
 
   def synchronize(files: Array[File]): Model = {
     val model = new LinkedHashModel
+    synchronize(files, model)
+    model
+  }
+
+  private def synchronize(files: Array[File], model: Model): Unit = {
     for (file <- files) {
-      FilenameUtils.getExtension(file.toString) match {
-        case "ics" => model.addAll(iCalConverter.convert(file))
-        case "vcf" => model.addAll(vCardConverter.convert(file))
-        case extension => throw new IOException("Unsupported file extension: " + extension)
+      if (file.isDirectory) {
+        synchronize(file.listFiles(), model)
+      } else {
+        synchronize(file, model)
       }
     }
-    model
+  }
+
+  private def synchronize(file: File, model: Model): Unit = {
+    FilenameUtils.getExtension(file.toString) match {
+      case "eml" => model.addAll(emailMessageConverter.convert(file))
+      case "ics" => model.addAll(iCalConverter.convert(file))
+      case "vcf" => model.addAll(vCardConverter.convert(file))
+      case extension => logger.info("Unsupported file extension " + extension + " for file " + file)
+    }
   }
 }

76ddab2dd61362aece0827ac42f98dc89c735b8a
Tpt <thomaspt@hotmail.fr>
2016-03-16 17:56:59 +0100

    Implements more method in SimpleHashModel

--- src/main/scala/pkb/rdf/model/SimpleHashModel.scala
+++ src/main/scala/pkb/rdf/model/SimpleHashModel.scala
@@ -6,24 +6,16 @@ import java.util.Optional
 import org.openrdf.model._
 import org.openrdf.model.impl.SimpleValueFactory
 
+import scala.collection.JavaConverters._
+
 /**
   * @author Thomas Pellissier Tanon
   */
-class SimpleHashModel(valueFactory: ValueFactory = SimpleValueFactory.getInstance(), statements: util.HashSet[Statement] = SimpleHashModel.emptySet)
+class SimpleHashModel(valueFactory: ValueFactory = SimpleValueFactory.getInstance(), statements: util.Set[Statement] = SimpleHashModel.emptySet)
   extends util.HashSet[Statement](statements) with Model {
 
   override def add(subj: Resource, pred: IRI, obj: Value, contexts: Resource*): Boolean = {
     toStatements(subj, pred, obj, contexts).map(add).reduce(_ || _)
-  }
-
-  private def toStatements(subj: Resource, pred: IRI, obj: Value, contexts: Seq[Resource]): Traversable[Statement] = {
-    if (contexts.isEmpty) {
-      List(valueFactory.createStatement(subj, pred, obj))
-    } else {
-      contexts.map(context =>
-        valueFactory.createStatement(subj, pred, obj, context)
-      )
-    }
   }
 
   override def getNamespaces: util.Set[Namespace] = {
@@ -46,6 +38,16 @@ class SimpleHashModel(valueFactory: ValueFactory = SimpleValueFactory.getInstanc
     toStatements(subj, pred, obj, contexts).map(remove).reduce(_ || _)
   }
 
+  private def toStatements(subj: Resource, pred: IRI, obj: Value, contexts: Seq[Resource]): Traversable[Statement] = {
+    if (contexts.isEmpty) {
+      List(valueFactory.createStatement(subj, pred, obj))
+    } else {
+      contexts.map(context =>
+        valueFactory.createStatement(subj, pred, obj, context)
+      )
+    }
+  }
+
   override def removeNamespace(prefix: String): Optional[Namespace] = {
     throw new UnsupportedOperationException
   }
@@ -55,23 +57,32 @@ class SimpleHashModel(valueFactory: ValueFactory = SimpleValueFactory.getInstanc
   }
 
   override def objects(): util.Set[Value] = {
-    throw new UnsupportedOperationException
+    this.asScala.map(_.getObject).asJava
   }
 
   override def predicates(): util.Set[IRI] = {
-    throw new UnsupportedOperationException
+    this.asScala.map(_.getPredicate).asJava
   }
 
   override def subjects(): util.Set[Resource] = {
-    throw new UnsupportedOperationException
+    this.asScala.map(_.getSubject).asJava
   }
 
   override def filter(subj: Resource, pred: IRI, obj: Value, contexts: Resource*): Model = {
-    throw new UnsupportedOperationException
+    doFilter(subj, pred, obj, contexts)
+  }
+
+  private def doFilter(subj: Resource, pred: IRI, obj: Value, contexts: Seq[Resource]): SimpleHashModel = {
+    new SimpleHashModel(valueFactory, this.asScala.filter(statement =>
+      (subj == null || statement.getSubject == subj) &&
+        (pred == null || statement.getPredicate == pred) &&
+        (obj == null || statement.getObject == subj) &&
+        (contexts.isEmpty || contexts.contains(statement.getContext))
+    ).asJava)
   }
 
   override def `match`(subj: Resource, pred: IRI, obj: Value, contexts: Resource*): util.Iterator[Statement] = {
-    throw new UnsupportedOperationException
+    doFilter(subj, pred, obj, contexts).iterator()
   }
 
   override def getValueFactory: ValueFactory = {

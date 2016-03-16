b11f029012d91e0baef2b6f78a83719a600ee6d1
Tpt <thomaspt@hotmail.fr>
2016-03-16 13:24:52 +0100

    Adds Google OAuth support to http API

--- src/main/scala/pkb/api/MainApi.scala
+++ src/main/scala/pkb/api/MainApi.scala
@@ -1,19 +1,27 @@
 package pkb.api
 
 import java.io.ByteArrayOutputStream
+import java.util.Properties
+import javax.mail.Session
 
 import akka.actor.ActorSystem
+import com.github.sardine.impl.SardineImpl
 import info.aduna.lang.FileFormat
 import info.aduna.lang.service.FileFormatServiceRegistry
+import org.apache.commons.io.IOUtils
+import org.openrdf.model.impl.SimpleValueFactory
 import org.openrdf.query._
 import org.openrdf.query.resultio.{BooleanQueryResultWriterRegistry, TupleQueryResultWriterRegistry}
 import org.openrdf.rio.RDFWriterRegistry
+import pkb.Pipeline
 import pkb.rdf.RepositoryFactory
+import pkb.sync.{CalDavSynchronizer, CardDavSynchronizer, EmailSynchronizer}
 import spray.http.HttpHeaders.{Accept, `Content-Type`}
 import spray.http._
 import spray.routing._
 
 import scala.compat.java8.OptionConverters._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * @author Thomas Pellissier Tanon
@@ -22,7 +30,9 @@ object MainApi extends App with SimpleRoutingApp {
 
   implicit val system = ActorSystem("pkb")
   val `application/sparql-query` = MediaTypes.register(MediaType.custom("application", "sparql-query", compressible = true))
+
   private val repositoryConnection = RepositoryFactory.initializedMemoryRepository.getConnection
+  private val pipeline = new Pipeline(repositoryConnection)
 
   startServer(interface = "localhost", port = 8080) {
     path("sparql") {
@@ -43,6 +53,26 @@ object MainApi extends App with SimpleRoutingApp {
               }
           }
       }
+    } ~
+      pathPrefix("oauth") {
+        pathPrefix("google") {
+          path("auth") {
+            redirect(OAuth2.Google.getAuthUri(Array(
+              "https://www.googleapis.com/auth/userinfo.email",
+              "https://www.googleapis.com/auth/carddav",
+              "https://www.googleapis.com/auth/calendar",
+              "https://mail.google.com/"
+            ), "http://localhost:8080/oauth/google/token"), StatusCodes.TemporaryRedirect) //TODO: avoid to hardcode the URI
+          } ~
+            path("token") {
+              parameter('code) { code =>
+                OAuth2.Google.getAccessToken(code, "http://localhost:8080/oauth/google/token").foreach(onGoogleToken)
+                complete {
+                  StatusCodes.OK
+                }
+              }
+            }
+        }
     }
   }
 
@@ -126,5 +156,33 @@ object MainApi extends App with SimpleRoutingApp {
       case Some(contentType) if contentType.contentType.mediaType.equals(expectedContentType.mediaType) => true
       case _ => false
     })
+  }
+
+  private def onGoogleToken(token: String): Unit = {
+    val sardine = new SardineImpl(token)
+
+    //Get user email
+    val gmailAddress = IOUtils.toString(sardine.get("https://www.googleapis.com/userinfo/email"))
+      .split("&")(0).split("=")(1) //The result has the format "email=foo@gmail.com&..."
+
+    //CardDav
+    pipeline.addSynchronizer(
+      new CardDavSynchronizer(SimpleValueFactory.getInstance, sardine, "https://www.googleapis.com/.well-known/carddav")
+    )
+
+    //CalDav
+    pipeline.addSynchronizer(
+      new CalDavSynchronizer(SimpleValueFactory.getInstance, sardine, "https://apidata.googleusercontent.com/caldav/v2/" + gmailAddress + "/events/")
+    )
+
+    //Emails
+    val props = new Properties()
+    props.put("mail.imap.ssl.enable", "true")
+    props.put("mail.imap.auth.mechanisms", "XOAUTH2")
+    val store = Session.getInstance(props).getStore("imap")
+    store.connect("imap.gmail.com", gmailAddress, token)
+    pipeline.addSynchronizer(new EmailSynchronizer(SimpleValueFactory.getInstance, store, 100))
+
+    pipeline.run(2) //TODO: bad hack
   }
 }

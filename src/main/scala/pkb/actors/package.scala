c7925f268ef882d104d962f1494b51689748239f
David Montoya <masda70@gmail.com>
2016-03-16 17:08:16 +0100

    - Added Error printing for Akka Streams

--- src/main/scala/pkb/actors/package.scala
+++ src/main/scala/pkb/actors/package.scala
@@ -1,13 +1,23 @@
 package pkb
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings, Supervision}
 
 /**
   * @author Thomas Pellissier Tanon
   * @author David Montoya
   */
 package object actors {
+
   implicit lazy val system = ActorSystem("pkb")
-  implicit lazy val materializer = ActorMaterializer()
+
+  lazy val decider: Supervision.Decider = {
+    case throwable =>
+      throwable.printStackTrace()
+      Supervision.Stop
+  }
+
+  implicit lazy val materializer = ActorMaterializer(
+    ActorMaterializerSettings(system).withSupervisionStrategy(decider))
+
 }

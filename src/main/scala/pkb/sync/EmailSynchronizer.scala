906df76c9e8d45d8a30ecef2e5a25ef55b41056f
Tpt <thomaspt@hotmail.fr>
2016-03-21 13:35:52 +0100

    Makes EmailSynchronizer more efficient
    Allows to add emails to the KB even if all emails are not converted

--- src/main/scala/pkb/sync/EmailSynchronizer.scala
+++ src/main/scala/pkb/sync/EmailSynchronizer.scala
@@ -11,6 +11,8 @@ import pkb.rdf.model.document.Document
 import pkb.sync.converter.EmailMessageConverter
 
 import scala.collection.mutable
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 /**
   * @author Thomas Pellissier Tanon
@@ -26,7 +28,7 @@ object EmailSynchronizer {
   private class Publisher(valueFactory: ValueFactory) extends ActorPublisher[Document] {
 
     private val emailMessageConverter = new EmailMessageConverter(valueFactory)
-    private val queue = new mutable.Queue[Document]
+    private val queue = new mutable.Queue[Future[Option[Document]]]
 
     override def receive: Receive = {
       case Request(_) =>
@@ -44,18 +46,31 @@ object EmailSynchronizer {
     private def retrieveMessages(folder: Folder): Unit = {
       folder.open(Folder.READ_ONLY)
       folder.getMessages.foreach(message => {
-        val document = Document(null, emailMessageConverter.convert(message, null))
-        if (waitingForData) {
-          onNext(document)
-        } else {
-          queue.enqueue(document)
+        val documentFuture = Future {
+          Some(Document(null, emailMessageConverter.convert(message, null)))
         }
-      }) //TODO: don't load all light messages at the same time but do batches
-      folder.close(false)
+        if (waitingForData) {
+          onNext(documentFuture)
+        } else {
+          queue.enqueue(documentFuture)
+        }
+      })
+      queue.enqueue(Future {
+        //We add to the queue the folder closing: it is executed only when all documents are set
+        folder.close(false)
+        None
+      })
     }
 
     private def waitingForData: Boolean = {
       isActive && totalDemand > 0
+    }
+
+    private def onNext(documentFuture: Future[Option[Document]]): Unit = {
+      documentFuture.foreach {
+        case Some(document) => onNext(document)
+        case None =>
+      }
     }
 
     private def deliverWaitingMessages(): Unit = {

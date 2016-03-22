f568d6517bab1cae46894c1ee2e3576c4f3bc69a
Tpt <thomaspt@hotmail.fr>
2016-03-22 14:09:28 +0100

    Adds a simple OWL inferencer

--- src/main/scala/pkb/rdf/RepositoryFactory.scala
+++ src/main/scala/pkb/rdf/RepositoryFactory.scala
@@ -4,8 +4,11 @@ import org.openrdf.IsolationLevels
 import org.openrdf.model.vocabulary.{RDF, RDFS}
 import org.openrdf.repository.sail.SailRepository
 import org.openrdf.repository.{Repository, RepositoryConnection}
+import org.openrdf.rio.RDFFormat
+import org.openrdf.sail.inferencer.fc.ForwardChainingRDFSInferencer
 import org.openrdf.sail.memory.MemoryStore
 import pkb.rdf.model.vocabulary.{Personal, SchemaOrg}
+import pkb.rdf.sail.inferencer.ForwardChainingSimpleOWLInferencer
 
 /**
   * @author Thomas Pellissier Tanon
@@ -15,9 +18,15 @@ object RepositoryFactory {
   def initializedMemoryRepository: Repository = {
     val store = new MemoryStore()
     store.setDefaultIsolationLevel(IsolationLevels.NONE)
-    val repository = new SailRepository(store)
+    val repository = new SailRepository(new ForwardChainingSimpleOWLInferencer(new ForwardChainingRDFSInferencer(store)))
+
     repository.initialize()
-    addNamespacesToRepository(repository.getConnection)
+
+    val repositoryConnection = repository.getConnection
+    addNamespacesToRepository(repositoryConnection)
+    loadOntology(repositoryConnection)
+    repositoryConnection.close()
+
     repository
   }
 
@@ -28,5 +37,13 @@ object RepositoryFactory {
     repositoryConnection.setNamespace(SchemaOrg.PREFIX, SchemaOrg.NAMESPACE)
     repositoryConnection.setNamespace(Personal.PREFIX, Personal.NAMESPACE)
     repositoryConnection.commit()
+  }
+
+  private def loadOntology(repositoryConnection: RepositoryConnection): Unit = {
+    repositoryConnection.add(
+      getClass.getClassLoader.getResourceAsStream("rdfs-ontology.ttl"),
+      Personal.NAMESPACE,
+      RDFFormat.TURTLE
+    )
   }
 }

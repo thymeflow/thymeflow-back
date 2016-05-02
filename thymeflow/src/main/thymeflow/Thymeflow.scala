4a3f3b59c59246cce3f73a16bfbed8a562dd8425
Tpt <thomaspt@hotmail.fr>
2016-05-02 16:32:24 +0200

    Adds DelayedBatch that allows Akka stream to send batches after a delay
    Uses it in the pipeline

--- thymeflow/src/main/thymeflow/Thymeflow.scala
+++ thymeflow/src/main/thymeflow/Thymeflow.scala
@@ -8,6 +8,7 @@ import thymeflow.rdf.RepositoryFactory
 import thymeflow.sync.FileSynchronizer
 import thymeflow.sync.converter.GoogleLocationHistoryConverter
 
+import scala.concurrent.duration._
 import scala.language.postfixOps
 
 /**
@@ -19,16 +20,18 @@ object Thymeflow extends StrictLogging {
   def main(args: Array[String]) {
     val repository = RepositoryFactory.initializedMemoryRepository(snapshotCleanupStore = false, owlInference = false, lucene = false)
     setupSynchronizers()
-    val pipeline = new Pipeline(repository.getConnection, List(
-      new InverseFunctionalPropertyInferencer(repository.getConnection),
-      new LocationStayEnricher(repository.getConnection),
-      new LocationEventEnricher(repository.getConnection),
-      new AgentIdentityResolutionEnricher(repository.getConnection)
-    ))
+    val pipeline = new Pipeline(
+      repository.getConnection,
+      Pipeline.enricherToFlow(new InverseFunctionalPropertyInferencer(repository.getConnection))
+        .via(Pipeline.delayedBatchToFlow(10 seconds))
+        .via(Pipeline.enricherToFlow(new LocationStayEnricher(repository.getConnection)))
+        .via(Pipeline.enricherToFlow(new LocationEventEnricher(repository.getConnection)))
+        .via(Pipeline.enricherToFlow(new AgentIdentityResolutionEnricher(repository.getConnection)))
+    )
+
     args.map(x => FileSynchronizer.Config(new File(x))).foreach {
       config => pipeline.addSource(config)
     }
-
   }
 
   def setupSynchronizers() = {
--- thymeflow/src/main/thymeflow/api/EfficientMainApi.scala
+++ thymeflow/src/main/thymeflow/api/EfficientMainApi.scala
@@ -6,6 +6,7 @@ import thymeflow.enricher.{AgentIdentityResolutionEnricher, InverseFunctionalPro
 import thymeflow.rdf.RepositoryFactory
 import thymeflow.{Pipeline, Thymeflow}
 
+import scala.concurrent.duration._
 import scala.language.postfixOps
 
 /**
@@ -21,12 +22,13 @@ object EfficientMainApi extends thymeflow.api.Api {
 
   override protected val pipeline = {
     Thymeflow.setupSynchronizers()
-    new Pipeline(repository.getConnection, List(
-      new InverseFunctionalPropertyInferencer(repository.getConnection),
-      new LocationStayEnricher(repository.getConnection),
-      new LocationEventEnricher(repository.getConnection),
-      new AgentIdentityResolutionEnricher(repository.getConnection))
+    new Pipeline(
+      repository.getConnection,
+      Pipeline.enricherToFlow(new InverseFunctionalPropertyInferencer(repository.getConnection))
+        .via(Pipeline.delayedBatchToFlow(10 seconds))
+        .via(Pipeline.enricherToFlow(new LocationStayEnricher(repository.getConnection)))
+        .via(Pipeline.enricherToFlow(new LocationEventEnricher(repository.getConnection)))
+        .via(Pipeline.enricherToFlow(new AgentIdentityResolutionEnricher(repository.getConnection)))
     )
   }
-
 }

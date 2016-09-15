ee06980576436c3cd2c1cf7891344ac344ba93a9
David Montoya <masda70@gmail.com>
2016-09-15 12:55:31 +0200

    Fix GeoCoordinatesConverterSpec test

--- core/src/test/com/thymeflow/sync/converter/utils/GeoCoordinatesConverterSpec.scala
+++ core/src/test/com/thymeflow/sync/converter/utils/GeoCoordinatesConverterSpec.scala
@@ -12,21 +12,40 @@ import scala.util.Random
   */
 class GeoCoordinatesConverterSpec extends FlatSpec with Matchers with StrictLogging {
 
+  private val geoUriRegex = """^geo:(-?\d+(?:\.\d+)?),(-?\d+(?:\.\d+)?)(?:,(-?\d+(?:\.\d+)?))?(?:;(?:crs=([\p{Alpha}\d-]+)))?(?:;(?:u=(\d+(?:\.\d+)?)))?""".r
+
   "GeoCoordinatesConverter" should "not lose precision" in {
+    val rand = new Random()
+    def generateRandomNumber(from: Double, to: Double, scale: Int) = {
+      BigDecimal(rand.nextDouble * (to - from) + from).setScale(scale, BigDecimal.RoundingMode.FLOOR).toDouble
+    }
     val valueFactory = SimpleValueFactory.getInstance()
     val converter = new GeoCoordinatesConverter(valueFactory)
-    val rand = new Random()
 
-    (1 to 10000).foreach {
-      i =>
-        val longitude = BigDecimal(rand.nextDouble * 360d - 180d).setScale(i % 16, BigDecimal.RoundingMode.FLOOR).toDouble
-        val latitude = BigDecimal(rand.nextDouble * 180d - 90d).setScale(i % 16, BigDecimal.RoundingMode.FLOOR).toDouble
-        val elevation = if (rand.nextBoolean) Some(BigDecimal(rand.nextDouble * 20000d - 10000d).setScale(i % 16, BigDecimal.RoundingMode.FLOOR).toDouble) else None
-        val uncertainty = if (rand.nextBoolean()) Some(BigDecimal(rand.nextDouble * 1000d).setScale(i % 16, BigDecimal.RoundingMode.FLOOR).toDouble) else None
-        val model = new SimpleHashModel()
-        val geo = converter.convert(longitude, latitude, elevation, uncertainty, model)
-        geo.stringValue() should be(s"geo:${latitude.toString},${longitude.toString}${elevation.map("," + _.toString).getOrElse("")}${uncertainty.map(";u=" + _.toString).getOrElse("")}")
-        logger.info(geo.stringValue())
+    try {
+      (1 to 10000).foreach {
+        i =>
+          val longitude = generateRandomNumber(-180d, 180d, i % 16)
+          BigDecimal(rand.nextDouble * 360d - 180d).setScale(i % 16, BigDecimal.RoundingMode.FLOOR).toDouble
+          val latitude = generateRandomNumber(-89d, 90d, i % 16)
+          val elevation = if (rand.nextBoolean) Some(generateRandomNumber(0d, 20000d, i % 16)) else None
+          val uncertainty = if (rand.nextBoolean) Some(generateRandomNumber(0d, 1000d, i % 16)) else None
+          val model = new SimpleHashModel()
+          val geo = converter.convert(longitude, latitude, elevation, uncertainty, model)
+          withClue(geo.stringValue()) {
+            geo.stringValue() match {
+              case geoUriRegex(geoLatitude, geoLongitude, geoElevation, geoCrsn, geoUncertainty) =>
+                geoLatitude.toDouble should be(latitude)
+                geoLongitude.toDouble should be(longitude)
+                Option(geoElevation).map(_.toDouble) should be(elevation)
+                Option(geoUncertainty).map(_.toDouble) should be(uncertainty)
+              case _ => fail(s"Could not parse 'geo' URI ${geo.stringValue()}")
+            }
+          }
+      }
+    } catch {
+      case e: NumberFormatException =>
+        fail(s"Could not parse number in 'geo' URI", e)
     }
   }
 }

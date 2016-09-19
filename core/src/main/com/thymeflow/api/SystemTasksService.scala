fd40861be4b40df6947b2a9133bf7ad731e95405
David Montoya <masda70@gmail.com>
2016-09-19 16:16:10 +0200

    system-tasks endpoint implements jsonApi

--- core/src/main/com/thymeflow/api/SystemTasksService.scala
+++ core/src/main/com/thymeflow/api/SystemTasksService.scala
@@ -4,7 +4,7 @@ import java.time.Instant
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.server.Directives
-import com.thymeflow.api.SystemTasksService.{JsonProtocol, Task, Tasks}
+import com.thymeflow.api.SystemTasksService._
 import com.thymeflow.utilities.JsonFormats
 import spray.json._
 
@@ -22,11 +22,11 @@ trait SystemTasksService extends Directives with CorsSupport {
     corsHandler {
       get {
         complete {
-          Tasks(List(
-            Task("loading", "Files: file.zip", Instant.now, "Done", Some(100)),
-            Task("loading", "CalDAV: angela@example.com", Instant.now, "In progress", Some((Instant.now.getEpochSecond % 100).toInt)),
-            Task("loading", "CardDAV: angela@example.com", Instant.now, "In progress", None),
-            Task("loading", "Email: angela@example2.com", Instant.now, "Waiting", Some(0))
+          ResourceObjects(Vector(
+            ResourceObject(Some("1"), "task", Task("loading", "Files: file.zip", Instant.now, "Done", Some(100))),
+            ResourceObject(Some("2"), "task", Task("loading", "CalDAV: angela@example.com", Instant.now, "In progress", Some((Instant.now.getEpochSecond % 100).toInt))),
+            ResourceObject(Some("3"), "task", Task("loading", "CardDAV: angela@example.com", Instant.now, "In progress", None)),
+            ResourceObject(Some("4"), "task", Task("loading", "Email: angela@example2.com", Instant.now, "Waiting", Some(0)))
           ))
         }
       }
@@ -38,12 +38,17 @@ object SystemTasksService {
 
   case class Task(`type`: String, name: String, startDate: Instant, status: String, progress: Option[Int])
 
-  case class Tasks(tasks: List[Task])
+  case class ResourceObject[T](id: Option[String], `type`: String, attributes: T)
+
+  case class ResourceObjects[T](data: scala.collection.immutable.Seq[ResourceObject[T]])
 
   object JsonProtocol extends DefaultJsonProtocol with JsonFormats.InstantJsonFormat {
     implicit val printer: CompactPrinter = CompactPrinter
-    implicit val taskFormat: JsonFormat[Task] = jsonFormat5(Task)
-    implicit val tasksFormat: RootJsonFormat[Tasks] = jsonFormat1(Tasks)
+    implicit val taskFormat: RootJsonFormat[Task] = jsonFormat5(Task)
+
+    implicit def resourceObjectFormat[T](implicit innerFormat: JsonFormat[T]): RootJsonFormat[ResourceObject[T]] = jsonFormat3(ResourceObject[T])
+
+    implicit def resourceObjectsFormat[T](implicit resourceObjectFormat: JsonFormat[ResourceObject[T]]): RootJsonFormat[ResourceObjects[T]] = jsonFormat1(ResourceObjects[T])
   }
 
 }

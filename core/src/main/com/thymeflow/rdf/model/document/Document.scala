cd783e8457d10ceabbe82dc2fd403f75861fbb03
David Montoya <masda70@gmail.com>
2017-05-12 11:27:32 -0700

    Require that all statements in a document have a context.

--- core/src/main/com/thymeflow/rdf/model/document/Document.scala
+++ core/src/main/com/thymeflow/rdf/model/document/Document.scala
@@ -6,4 +6,6 @@ import org.eclipse.rdf4j.model.IRI
 /**
   * @author Thomas Pellissier Tanon
   */
-case class Document(iri: IRI, statements: StatementSet)
+case class Document(iri: IRI, statements: StatementSet){
+  require(statements.forall(s => s.getContext != null), s"All statements within a document must have a context. Found ${statements.find(_.getContext == null)} instead.")
+}

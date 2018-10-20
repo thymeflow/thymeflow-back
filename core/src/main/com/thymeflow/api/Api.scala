41801f8ef2a301a4842c69dcd1b839bc64c1bc2f
David Montoya <masda70@gmail.com>
2018-10-20 16:28:55 +0200

    reformat code

--- core/src/main/com/thymeflow/api/Api.scala
+++ core/src/main/com/thymeflow/api/Api.scala
@@ -25,7 +25,9 @@ trait Api extends SparqlService with SystemTasksService with DataServicesService
   protected implicit val actorSystemContext: ActorSystemContext
 
   import actorSystemContext.Implicits._
+
   protected implicit def config: Config
+
   private val backendUri = Uri(config.getString("thymeflow.http.backend-uri").replaceAll("/$", ""))
   private val frontendUri = Uri(config.getString("thymeflow.http.frontend-uri"))
   protected override val allowedOrigin = frontendUri.withPath(Path.Empty).toString
@@ -112,6 +114,7 @@ trait Api extends SparqlService with SystemTasksService with DataServicesService
   protected def repository: Repository
 
   private val executionStart: Long = System.currentTimeMillis()
+
   protected def durationSinceStart: Duration = {
     Duration(System.currentTimeMillis() - executionStart, TimeUnit.MILLISECONDS)
   }
@@ -150,6 +153,7 @@ trait Api extends SparqlService with SystemTasksService with DataServicesService
         }
       }
     }
+
     val prefixedRoute = segments(backendUri.path).foldLeft(route) {
       case (nestedRoute, segment) =>
         pathPrefix(segment) {
